8dffd090ed9ae4c374fc4e4b6677def69a7dbf1e
ekarat <pokekarat@gmail.com>
2019-10-13 20:38:39 +0700

    postgamejam1_tutorial

--- LD45_Game/Assets/Script/MainMenu.cs
+++ LD45_Game/Assets/Script/MainMenu.cs
@@ -8,4 +8,13 @@ public class MainMenu : MonoBehaviour
     public void playGame(){
        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
    }
+
+   public void gotoTutorial(){
+       SceneManager.LoadScene("Tutorial");
+   }
+
+   public void gotoMainMenu(){
+       Debug.Log("menu");
+       SceneManager.LoadScene("Menu");
+   }
 }
